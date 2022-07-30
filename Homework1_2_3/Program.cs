c8674ce35d9701a89e6bf852feb4a8e30a8b1bca
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-07-30 21:49:27 +0300

    Добавьте файлы проекта.

---
+++ Homework1_2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=5;i<100;i+=7)
+            {
+                Console.Write(i+" ");
+            }
+        }
+    }
+}
